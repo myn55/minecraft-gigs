903d70ba882957cd5d3fe6834057605b551cd8e5
Coronhaus <coronhaus@gmail.com>
2020-12-29 23:55:55 -0500

    Added writeCenter function

---
+++ computercraft/modules/uiutils.lua
@@ -0,0 +1,16 @@
+-- Author: Coronhaus
+local module = {}
+
+local width, height = term.getSize()
+
+function module.writeCenter(y, text, writeSlow, speed)
+    local x = math.floor((width-#text)/2)
+    term.setCursorPos(x, y)
+    if writeSlow then
+        textUtils.slowWrite(text, speed)
+    else
+        term.write(text)
+    end
+end
+
+return module
