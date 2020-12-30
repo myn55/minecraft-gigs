5bf6398b1839457129b64ab5127dc580a66e965a
Coronhaus <coronhaus@gmail.com>
2020-12-30 12:05:19 -0500

    Added loading bar

--- computercraft/password-lock/computer/startup.lua
+++ computercraft/password-lock/computer/startup.lua
@@ -4,7 +4,7 @@
 --os.pullEvent = os.pullEventRaw
 
 -- Constants
-local settings = require '.password_settings'
+local settings = require 'password_settings'
 local width, height = term.getSize()
 
 -- Loading screen rendering
@@ -13,11 +13,25 @@ term.setCursorPos(1, 1)
 term.clearLine()
 textutils.slowWrite('Endemal Systems //', 15)
 term.setCursorPos(1, 2)
-term.setTextColor(colors.yellow)
 textutils.slowWrite('Loading...', 15)
+
 sleep(0.5)
+
+term.setTextColor(colors.yellow)
+term.setCursorPos(10, 10)
+for i=1, width-19 do
+    term.write('#')
+    sleep(0.05)
+end
+sleep(0.5)
+
 term.setTextColor(colors.green)
+term.setCursorPos(1, 2)
 term.clearLine()
 term.write('Loading complete')
+if settings.speakerInstalled then peripheral.call(settings.speakerInstalled, 'playSound(\'minecraft:entity.player.levelup\')') end
+sleep(0.75)
+term.setCursorPos(1, 10)
+term.clearLine()
 
 shell.run('password.lua')
