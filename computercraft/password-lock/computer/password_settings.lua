5987f4450a47905b1de65c85630e090e39c13859
Coronhaus <coronhaus@gmail.com>
2020-12-30 11:33:57 -0500

    Added more settings

---
+++ computercraft/password-lock/computer/password_settings.lua
@@ -0,0 +1,11 @@
+-- Author: Coronhaus
+-- Auto-generated password settings by the installer
+
+local module = {
+    terminal = false; -- If this computer is solely made to be a password terminal outputting a redstone signal from a side
+    redstoneSide = ''; -- If a terminal, the side for the redstone signal to go on
+    redstoneStrength = 0; -- Strength of the redstone signal
+    speakerInstalled = false; -- If a speaker is installed on any side
+}
+
+return module
