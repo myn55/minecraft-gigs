f6cdb61d86b2dd55c3bea747079eb8f4879b63b8
Coronhaus <coronhaus@gmail.com>
2020-12-30 11:29:09 -0500

    Renamed without the first period

--- computercraft/password-lock/computer/.password_settings.lua
+++
@@ -1,8 +0,0 @@
--- Author: Coronhaus
--- Auto-generated password settings by the installer
-
-local module = {
-    
-}
-
-return module
--- computercraft/password-lock/computer/password.lua
+++ computercraft/password-lock/computer/password.lua
@@ -1,2 +1,5 @@
 -- Author: Coronhaus
 
+term.setCursorPos(1, 2)
+term.clearLine()
+term.setCursorPos(1, 3)
